5538d0ec1215401158156f90dd3f5c6cac171e10
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-02 03:20:20 +0300

    signatured userreapository to user

--- src/main/java/org/curs/AppServer/repository/UserRepository.java
+++ src/main/java/org/curs/AppServer/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package org.curs.AppServer.repository;
 
-import org.curs.AppServer.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Customer,Integer> {
+import org.curs.AppServer.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByLogin(String login);
 }
