8e5251352bcd9700ea53d05ee22c3f402621d3de
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-05 11:11:05 +0300

    made primaryStage static

--- src/main/java/org/curs/AppClient/JavaFXApp.java
+++ src/main/java/org/curs/AppClient/JavaFXApp.java
@@ -3,7 +3,7 @@ package org.curs.AppClient;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import lombok.Getter;
-import org.curs.AppClient.ScenesControllers.WelcomeSceneController;
+import org.curs.AppClient.ScenesControllers.CommonControllers.WelcomeSceneController;
 import org.curs.AppServer.TvApp;
 import org.curs.AppClient.ScenesControllers.SceneManager;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +15,7 @@ public class JavaFXApp extends Application {
     private ConfigurableApplicationContext context;
     //Сцена для отрисовки в других классах
     @Getter
-    private Stage primaryStage;
+    private static Stage primaryStage;
 
     //Запускаю сервер перед javaFx
     @Override
