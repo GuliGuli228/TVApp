7cf02fc387c95b648c5335bbc3e1b6291a664665
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-05 18:54:34 +0300

    added method call for cache

--- src/main/java/org/curs/AppClient/ScenesControllers/CommonControllers/WelcomeSceneController.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/CommonControllers/WelcomeSceneController.java
@@ -7,7 +7,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import org.curs.AppClient.AppContext;
+import org.curs.AppClient.AppCache;
 import org.curs.AppClient.JavaFXApp;
 import org.curs.AppClient.ScenesControllers.AdminControllers.AdminContractController;
 import org.curs.AppClient.ScenesControllers.AgentContollers.AgentContractController;
@@ -42,18 +42,21 @@ public class WelcomeSceneController {
         connection = ApiUtil.fetchApi("/api/v1/user/login?login="+login+"&password="+password, ApiUtil.RequestMethod.POST,null);
 
         if(connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-            JsonObject user = ApiUtil.getUserData("/api/v1/user/login", login,password);
-            String role = user.get("role").getAsString();
+            JsonObject user = ApiUtil.getUserData("/api/v1/user/login", login);
+            System.out.println(user);
 
-            System.out.println(user.toString());
-            AppContext.setUserId(user.get("id").getAsInt());
-            AppContext.setUserName(user.get("login").getAsString());
-            AppContext.setRole(role);
+            String role = user.get("role").getAsString();
+            AppCache.setRole(role);
+            AppCache.setUserId(user.get("id").getAsInt());
+            AppCache.setUserName(user.get("login").getAsString());
+
+            AppCache.loadCache();
+
 
             if(role.equals("Admin")) SceneManager.showScene("AdminScene", JavaFXApp.getPrimaryStage(),new AdminContractController());
             if(role.equals("Agent"))SceneManager.showScene("AgentScene", JavaFXApp.getPrimaryStage(),new AgentContractController());
         }
-        else{
+        else{//FIXME
             if (connection.getResponseCode() == HttpURLConnection.HTTP_UNAUTHORIZED) {
                 System.out.println(connection.getResponseCode());
                 WelcomeSceneController.showAlert("You are not authorized");
