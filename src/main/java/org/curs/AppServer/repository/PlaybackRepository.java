c679932886eaba9cdde222cb69ab0a558c122223
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-05 18:49:58 +0300

    changed methods due to changes in DB

--- src/main/java/org/curs/AppServer/repository/PlaybackRepository.java
+++ src/main/java/org/curs/AppServer/repository/PlaybackRepository.java
@@ -3,5 +3,9 @@ package org.curs.AppServer.repository;
 import org.curs.AppServer.entities.Playback;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface PlaybackRepository extends JpaRepository<Playback,Integer> {
+    Optional<List<Playback>> findAllByContract_Id(Integer id);
 }
--- src/main/java/org/curs/AppServer/repository/TelecastRepository.java
+++ src/main/java/org/curs/AppServer/repository/TelecastRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TelecastRepository extends JpaRepository<Telecast,Integer> {
-    Optional<Telecast> findByPlaybackId(Integer playbackId);
 }
