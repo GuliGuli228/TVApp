788391fd753a018505a8bf0c55b5ca7c850457bf
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-05 11:34:24 +0300

    added method for switching between scenes

--- src/main/java/org/curs/AppClient/ScenesControllers/SceneManager.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/SceneManager.java
@@ -28,4 +28,42 @@ public class SceneManager {
         stage.setScene(scene);
         stage.show();
     }
+    public static void switchScene(Scenes scene){
+        String roleName = AppContext.getRole();
+        Object controller = null;
+        switch (scene){
+            case ADMIN_CONTRACTS -> controller = new AdminContractController();
+            case ADMIN_CUSTOMERS -> controller =new AdminCustomersController();
+            case ADMIN_PLAYBACK -> controller =new AdminPlaybackController();
+            case AGENTS -> controller =new AdminAgentsController();
+            case FINANCES -> controller =new FinancesController();
+            case AGENT_CONTRACTS -> controller =new AgentContractController();
+            case AGENT_PLAYBACK -> controller =new AgentPlaybackController();
+            case AGENT_CUSTOMERS -> controller =new AgentCustomersController();
+            case PROMO -> controller =new PromoController();
+            case TELECAST -> controller =new TelecastController();
+        }
+        if (roleName.equals("Admin")) SceneManager.showScene("AdminScene", JavaFXApp.getPrimaryStage(), controller);
+        if (roleName.equals("Agent")) SceneManager.showScene("AgentScene", JavaFXApp.getPrimaryStage(), controller);
+    }
+
+    public static void showDialog(String dialogName, Stage ownerStage,Object controller)  {
+        Stage stage = new Stage();
+        Parent root = null;
+
+        try {
+            FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(SceneManager.class.getResource(dialogName)));
+            loader.setController(controller);
+            root = loader.load();
+        } catch (IOException e) {
+            System.err.println("Error loading Dialog" + e.getMessage());
+        }
+
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setResizable(false);
+        stage.initOwner(ownerStage);
+        stage.showAndWait();
+    }
 }
