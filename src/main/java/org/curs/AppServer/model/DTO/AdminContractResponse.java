effe8f1206ea5560f9c7ad858aa74b29ff7f5ff3
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-07 23:55:19 +0300

    changed class to record

--- src/main/java/org/curs/AppServer/model/DTO/AdminContractResponse.java
+++ src/main/java/org/curs/AppServer/model/DTO/AdminContractResponse.java
@@ -1,16 +1,4 @@
 package org.curs.AppServer.model.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.sql.Time;
-import java.util.Date;
-
-@AllArgsConstructor
-@Data
-public class AdminContractResponse {
-    private Integer contractId;
-    private Integer AgentId;
-    private Integer CustomerId;
-    private Double price;
-}
+public record   AdminContractResponse (Integer contractId,Integer agentId,Integer customerId,Double price)
+{ }
