33f3c24cb1b6ffd7e6e2cfba82fccc59ed3718a0
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-09 19:13:18 +0300

    completed rest controller for agent

---
+++ src/main/java/org/curs/AppServer/controllers/AgentController.java
@@ -0,0 +1,29 @@
+package org.curs.AppServer.controllers;
+
+import org.curs.AppServer.entities.Agent;
+import org.curs.AppServer.model.DTO.AdminAgentResponse;
+import org.curs.AppServer.service.AgentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/agent")
+public class AgentController {
+
+    @Autowired
+    private AgentService agentService;
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<AdminAgentResponse>> getAll() {
+        Optional<List<AdminAgentResponse>> agents = agentService.findAll();
+        if(agents.isPresent()) return new ResponseEntity<>(agents.get(), HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
