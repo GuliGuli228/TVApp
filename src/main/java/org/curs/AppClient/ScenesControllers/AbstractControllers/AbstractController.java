94c8c5f9114b9d8941cf16bb58045565d76fba44
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-10 08:58:04 +0300

    Changed idea from 3 abstract controllers (AdminAbstract, AgentAbstract, CommonAbstract) to one abstract controller

---
+++ src/main/java/org/curs/AppClient/ScenesControllers/AbstractControllers/AbstractController.java
@@ -0,0 +1,114 @@
+package org.curs.AppClient.ScenesControllers.AbstractControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.curs.AppClient.AppCache;
+import org.curs.AppClient.ScenesControllers.SceneManager;
+import org.curs.AppClient.ScenesControllers.Scenes;
+
+import java.util.Objects;
+import java.util.logging.Logger;
+
+public class AbstractController {
+
+    Logger logger = Logger.getLogger(this.getClass().getName());
+
+    //(required = false)
+    /*---Admin Buttons---*/
+    @FXML
+    protected Button AdminAddButton;
+    @FXML
+    protected Button AdminContractButton;
+    @FXML
+    protected Button AdminPlaybackButton;
+    @FXML
+    protected Button AdminCustomersButton;
+    @FXML
+    protected Button AdminPromosButton;
+    @FXML
+    protected Button AdminAgentsButton;
+    @FXML
+    protected Button AdminTelecastButton;
+    /*-------------------*/
+
+    //(required = false)
+    /*---Agent Buttons----*/
+    @FXML
+    protected Button AgentContractButton;
+    @FXML
+    protected Button AgentPlaybackButton;
+    @FXML
+    protected Button AgentCustomersButton;
+    @FXML
+    protected Button AgentPromoButton;
+    @FXML
+    protected Button AgentTelecastButton;
+    /*-------------------*/
+
+    @FXML
+    public void initialize() {
+        if(Objects.equals(AppCache.getRole(), "Admin")) {
+            AdminContractButton.setOnAction(this::SwitchToAdminContract);
+            AdminPlaybackButton.setOnAction(this::SwitchToAdminPlayback);
+            AdminCustomersButton.setOnAction(this::SwitchToAdminCustomers);
+            AdminAgentsButton.setOnAction(this::SwitchToAdminAgents);
+            AdminTelecastButton.setOnAction(this::SwitchToTelecast);
+            AdminPromosButton.setOnAction(this::SwitchToPromo);
+        }
+        if(Objects.equals(AppCache.getRole(), "Agent")) {
+            AgentContractButton.setOnAction(this::SwitchToAgentContract);
+            AgentPlaybackButton.setOnAction(this::SwitchToAgentPlayback);
+            AgentCustomersButton.setOnAction(this::SwitchToAgentCustomers);
+            AgentPromoButton.setOnAction(this::SwitchToPromo);
+            AgentTelecastButton.setOnAction(this::SwitchToTelecast);
+        }
+    }
+    /*--- Admin Scenes--*/
+    protected   void SwitchToAdminContract(ActionEvent event)  {
+        logger.info("switching to Admin Contract");
+        SceneManager.switchScene(Scenes.ADMIN_CONTRACTS);
+    }
+    protected void SwitchToAdminPlayback(ActionEvent event)  {
+        logger.info("Switching to Admin Playback");
+        SceneManager.switchScene(Scenes.ADMIN_PLAYBACK);
+    }
+    protected void SwitchToAdminCustomers(ActionEvent event)  {
+        logger.info("Switching to Admin Customers");
+        SceneManager.switchScene(Scenes.ADMIN_CUSTOMERS);
+    }
+    protected void SwitchToAdminAgents(ActionEvent event)  {
+        logger.info("Switching to Admin Agents");
+        SceneManager.switchScene(Scenes.AGENTS);
+    }
+    /*------------------*/
+
+
+    /*--- Agent Scenes--*/
+    protected   void SwitchToAgentContract(ActionEvent event)  {
+        logger.info("switching to Agent ontract");
+        SceneManager.switchScene(Scenes.AGENT_CONTRACTS);
+    }
+    protected void SwitchToAgentPlayback(ActionEvent event)  {
+        logger.info("Switching to Agent Playback");
+        SceneManager.switchScene(Scenes.AGENT_PLAYBACK);
+    }
+    protected void SwitchToAgentCustomers(ActionEvent event)  {
+        logger.info("Switching to Agent Customers");
+        SceneManager.switchScene(Scenes.AGENT_CUSTOMERS);
+    }
+    /*------------------*/
+
+
+    /*---Common Scenes---*/
+    protected void SwitchToPromo(ActionEvent event)  {
+        logger.info("Switching to Telecast");
+        SceneManager.switchScene(Scenes.TELECAST);
+    }
+    protected void SwitchToTelecast(ActionEvent event)  {
+        logger.info("Switching to Promo");
+        SceneManager.switchScene(Scenes.PROMO);
+    }
+    /*------------------*/
+
+}
