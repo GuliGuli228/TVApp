7057db26bd1e464a52f662b4d771fce4f84b602d
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-12 14:12:29 +0300

    made view for agent

--- src/main/java/org/curs/AppClient/ScenesControllers/AgentContollers/AgentContractController.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/AgentContollers/AgentContractController.java
@@ -1,4 +1,37 @@
 package org.curs.AppClient.ScenesControllers.AgentContollers;
 
-public class AgentContractController {
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.curs.AppClient.AppCache;
+import org.curs.AppClient.ScenesControllers.AbstractControllers.AbstractController;
+
+import java.util.List;
+
+public class AgentContractController extends AbstractController {
+
+    @FXML
+    private TableView<AppCache.AgentContractResponse> TableBox;
+
+    @FXML
+    public void initialize() {
+        super.initialize();
+        List<AppCache.AgentContractResponse> agentContractResponses = AppCache.getAgentContractResponses();
+        System.out.println("agentContractResponses: " + agentContractResponses);
+
+        TableColumn<AppCache.AgentContractResponse, Integer> tableColumnContractId = new TableColumn<>("ID Контракта");
+        TableColumn<AppCache.AgentContractResponse, Integer> tableColumnCustomerId = new TableColumn<>("ID Заказчика");
+        TableColumn<AppCache.AgentContractResponse, Double> tableColumnPrice = new TableColumn<>("Стоимость");
+
+        tableColumnContractId.setCellValueFactory(cell -> new ReadOnlyObjectWrapper<>(cell.getValue().contractId()));
+        tableColumnCustomerId.setCellValueFactory(cell -> new ReadOnlyObjectWrapper<>(cell.getValue().customerId()));
+        tableColumnPrice.setCellValueFactory(cell -> new ReadOnlyObjectWrapper<>(cell.getValue().price()));
+
+        TableBox.getColumns().addAll(tableColumnContractId, tableColumnCustomerId, tableColumnPrice);
+        ObservableList<AppCache.AgentContractResponse> responses = FXCollections.observableList(agentContractResponses);
+        TableBox.setItems(responses);
+    }
 }
--- src/main/java/org/curs/AppClient/ScenesControllers/AgentContollers/AgentCustomersController.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/AgentContollers/AgentCustomersController.java
@@ -1,4 +1,44 @@
 package org.curs.AppClient.ScenesControllers.AgentContollers;
 
-public class AgentCustomersController {
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.curs.AppClient.AppCache;
+import org.curs.AppClient.ScenesControllers.AbstractControllers.AbstractController;
+
+import java.util.List;
+
+public class AgentCustomersController extends AbstractController {
+
+    @FXML
+    private TableView<AppCache.CustomersResponse> TableBox;
+
+    @FXML
+    public void initialize() {
+        super.initialize();
+
+        List<AppCache.CustomersResponse> customersResponses = AppCache.getCustomersResponses();
+        System.out.println("customersResponses: " + customersResponses);
+
+        TableColumn<AppCache.CustomersResponse, Integer> tableColumnCustomerId = new TableColumn<>("ID Заказчика");
+        TableColumn<AppCache.CustomersResponse, String> tableColumnCustomerIban = new TableColumn<>("Банковские реквизиты");
+        TableColumn<AppCache.CustomersResponse, String> tableColumnCustomerPhone = new TableColumn<>("Номер телефона");
+        TableColumn<AppCache.CustomersResponse, String> tableColumnCustomerContactPerson = new TableColumn<>("Контактное лицо");
+        TableColumn<AppCache.CustomersResponse, Integer> tableColumnCustomerAmountOfContracts = new TableColumn<>("Количество контрактов");
+        TableColumn<AppCache.CustomersResponse, Double> tableColumnCustomerPriceOfContracts = new TableColumn<>("Стоимость всех контрактов");
+
+        tableColumnCustomerId.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(cell.getValue().customerId()));
+        tableColumnCustomerIban.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(cell.getValue().iban()));
+        tableColumnCustomerPhone.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(cell.getValue().phone()));
+        tableColumnCustomerContactPerson.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(cell.getValue().contactPerson()));
+        tableColumnCustomerAmountOfContracts.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(cell.getValue().amountOfContracts()));
+        tableColumnCustomerPriceOfContracts.setCellValueFactory(cell-> new ReadOnlyObjectWrapper<>(cell.getValue().priceOfContracts()));
+
+        TableBox.getColumns().addAll(tableColumnCustomerId,tableColumnCustomerIban, tableColumnCustomerPhone,tableColumnCustomerContactPerson,tableColumnCustomerAmountOfContracts,tableColumnCustomerPriceOfContracts );
+        ObservableList<AppCache.CustomersResponse> list = FXCollections.observableArrayList(customersResponses);
+        TableBox.setItems(list);
+    }
 }
