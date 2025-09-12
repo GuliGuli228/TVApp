c8cb6e837ce51e969da315dbd24abac7860d475b
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-12 14:08:23 +0300

    made playback common

--- src/main/java/org/curs/AppClient/ScenesControllers/Scenes.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/Scenes.java
@@ -1,7 +1,7 @@
 package org.curs.AppClient.ScenesControllers;
 
 public enum Scenes {
-    ADMIN_CONTRACTS, ADMIN_CUSTOMERS,AGENTS,ADMIN_PLAYBACK, // Экраны Админа
-    AGENT_CONTRACTS, AGENT_PLAYBACK, AGENT_CUSTOMERS, // Экраны агента
-    PROMO,TELECAST //Общие экраны
+    ADMIN_CONTRACTS, ADMIN_CUSTOMERS,AGENTS, // Экраны Админа
+    AGENT_CONTRACTS,  AGENT_CUSTOMERS, // Экраны агента
+    PLAYBACK, PROMO,TELECAST //Общие экраны
 }
