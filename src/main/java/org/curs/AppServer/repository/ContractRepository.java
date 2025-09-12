ba183f2f0ab689e191e94df6f49f805e2a793575
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-12 14:11:52 +0300

    added JPAs and JPQL for agent data

--- src/main/java/org/curs/AppServer/repository/ContractRepository.java
+++ src/main/java/org/curs/AppServer/repository/ContractRepository.java
@@ -1,14 +1,14 @@
 package org.curs.AppServer.repository;
 
 import org.curs.AppServer.entities.Contract;
+import org.curs.AppServer.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ContractRepository extends JpaRepository<Contract,Integer> {
     Optional<List<Contract>> findAllByAgentId(Integer id);
-
     Optional<List<Contract>> findAllByCustomerId(Integer customerId);
+    Optional<List<Contract>> findAllByAgentIdAndCustomerId(Integer agentId,Integer customerId);
 }
--- src/main/java/org/curs/AppServer/repository/CustomerRepository.java
+++ src/main/java/org/curs/AppServer/repository/CustomerRepository.java
@@ -2,7 +2,14 @@ package org.curs.AppServer.repository;
 
 import org.curs.AppServer.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer,Integer> {
 
+    @Query("SELECT DISTINCT c.customer FROM Contract c WHERE c.agent.id = :agentId")
+    Optional<List<Customer>> findDistinctCustomersByAgentId(@Param("agentId") Integer agentId);
 }
