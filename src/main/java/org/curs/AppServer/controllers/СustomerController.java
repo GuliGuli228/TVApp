0078140f2bbb227224287da8d05036286fa7df44
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-12 14:17:46 +0300

    made correct Controller

--- src/main/java/org/curs/AppServer/controllers/СustomerController.java
+++ src/main/java/org/curs/AppServer/controllers/СustomerController.java
@@ -1,17 +1,15 @@
 package org.curs.AppServer.controllers;
 
-import org.curs.AppServer.entities.Customer;
-import org.curs.AppServer.model.DTO.AdminCustomersResponse;
-import org.curs.AppServer.repository.CustomerRepository;
+import org.curs.AppServer.model.DTO.CommonResponses.CustomersResponse;
 import org.curs.AppServer.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,9 +21,17 @@ public class СustomerController {
     private CustomerService customerService;
 
     @GetMapping("/getAll")
-    public ResponseEntity<List<AdminCustomersResponse>> getAllCustomers() {
-        Optional<List<AdminCustomersResponse>> customers = customerService.getAllCustomers();
+    public ResponseEntity<List<CustomersResponse>> getAllCustomers() {
+        Optional<List<CustomersResponse>> customers = customerService.getAllCustomers();
         if(customers.isPresent()) return new ResponseEntity<>(customers.get(), HttpStatus.OK);
         else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+    @GetMapping("/getAllForAgent")
+    public ResponseEntity<List<CustomersResponse>> getAllForAgent(@RequestParam Integer agentId) {
+        Optional<List<CustomersResponse>> customers = customerService.getAllCustomersForAgentById(agentId);
+        if(customers.isPresent()) return new ResponseEntity<>(customers.get(), HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+
 }
