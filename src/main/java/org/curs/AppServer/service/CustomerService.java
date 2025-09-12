ed5dd867808a9d68e6d1f4e93d927203ae129369
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-12 14:16:23 +0300

    made customerResponse common

--- src/main/java/org/curs/AppServer/service/CustomerService.java
+++ src/main/java/org/curs/AppServer/service/CustomerService.java
@@ -2,7 +2,7 @@ package org.curs.AppServer.service;
 
 import org.curs.AppServer.entities.Contract;
 import org.curs.AppServer.entities.Customer;
-import org.curs.AppServer.model.DTO.AdminCustomersResponse;
+import org.curs.AppServer.model.DTO.CommonResponses.CustomersResponse;
 import org.curs.AppServer.repository.ContractRepository;
 import org.curs.AppServer.repository.CustomerRepository;
 import org.curs.AppServer.repository.PlaybackRepository;
@@ -23,9 +23,9 @@ public class CustomerService {
     @Autowired
     private PlaybackRepository playbackRepository;
 
-    public Optional<List<AdminCustomersResponse>> getAllCustomers(){
+    public Optional<List<CustomersResponse>> getAllCustomers(){
         Optional<List<Customer>> customers = Optional.of(customerRepository.findAll());
-        List<AdminCustomersResponse> response = new ArrayList<>();
+        List<CustomersResponse> response = new ArrayList<>();
         for(Customer customer : customers.get()){
             Optional<List<Contract>> contracts = contractRepository.findAllByCustomerId(customer.getId());
 
@@ -44,7 +44,29 @@ public class CustomerService {
                     .mapToDouble(list -> list.stream()
                             .flatMap(List::stream)
                             .mapToDouble(playback -> playback.getPromo().getDuration().toMinutes() * playback.getTelecast().getMinuteCost()).sum()).sum();
-            response.add(new AdminCustomersResponse(customerId, iban, phone, contactPerson, amountOfContracts, priceOfContracts));
+            response.add(new CustomersResponse(customerId, iban, phone, contactPerson, amountOfContracts, priceOfContracts));
+        }
+        return Optional.of(response);
+    }
+
+    public Optional<List<CustomersResponse>> getAllCustomersForAgentById(Integer agentId){
+        Optional<List<Customer>> customers = customerRepository.findDistinctCustomersByAgentId(agentId);
+        List<CustomersResponse> response = new ArrayList<>();
+
+        for(Customer customer : customers.get()){
+            Optional<List<Contract>> contracts = contractRepository.findAllByAgentIdAndCustomerId(agentId,customer.getId());
+            Integer customerId = customer.getId();
+            String iban = customer.getIban();
+            String phone = customer.getPhone();
+            String contactPerson = customer.getContactPerson();
+            Integer amountOfContracts = contracts.get().size();
+            Double price  = contracts.stream()
+                    .flatMap(List::stream)
+                    .map(contract -> playbackRepository.findAllByContract_Id(contract.getId()))
+                    .mapToDouble(list ->list.stream()
+                            .flatMap(List::stream)
+                            .mapToDouble(playback -> playback.getPromo().getDuration().toMinutes() * playback.getTelecast().getMinuteCost()).sum()).sum();
+            response.add(new CustomersResponse(customerId, iban, phone, contactPerson, amountOfContracts, price));
         }
         return Optional.of(response);
     }
