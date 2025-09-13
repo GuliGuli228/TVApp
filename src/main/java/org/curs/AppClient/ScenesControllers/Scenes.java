2c9b3daacea58cf2fa99551b00ef16bc8745bab8
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-13 12:24:53 +0300

    added account to common

--- src/main/java/org/curs/AppClient/ScenesControllers/Scenes.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/Scenes.java
@@ -3,5 +3,5 @@ package org.curs.AppClient.ScenesControllers;
 public enum Scenes {
     ADMIN_CONTRACTS, ADMIN_CUSTOMERS,AGENTS, // Экраны Админа
     AGENT_CONTRACTS,  AGENT_CUSTOMERS, // Экраны агента
-    PLAYBACK, PROMO,TELECAST //Общие экраны
+    ACCOUNT,PLAYBACK, PROMO,TELECAST //Общие экраны
 }
