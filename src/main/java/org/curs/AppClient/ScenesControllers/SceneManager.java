a7495c1f903ea36640d02ab86cec8462a5102daf
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-15 16:16:16 +0300

    updated method for dialog

--- src/main/java/org/curs/AppClient/ScenesControllers/SceneManager.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/SceneManager.java
@@ -1,10 +1,13 @@
 package org.curs.AppClient.ScenesControllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.curs.AppClient.AppCache;
 import org.curs.AppClient.JavaFXApp;
 import org.curs.AppClient.ScenesControllers.AdminControllers.*;
@@ -25,6 +28,7 @@ public class SceneManager {
     private static final Logger logger = Logger.getLogger(SceneManager.class.getName());
 
     private final static Map<String, String> scenePaths = new HashMap<>();
+    private final static Map<String, String> dialogPaths = new HashMap<>();
 
 
     // Добавляю все сцены и их пути, чтобы можно было переключаться между сценами по их названиям
@@ -33,6 +37,12 @@ public class SceneManager {
             scenePaths.put("AgentScene", "/FXMLScenes/AgentScene.fxml");
             scenePaths.put("AdminScene", "/FXMLScenes/AdminScene.fxml");
             scenePaths.put("AccountScene", "/FXMLScenes/AccountScene.fxml");
+
+            dialogPaths.put("AddAgent", "/FXMLScenes/AddAgentDialog.fxml");
+            dialogPaths.put("AddContract", "FXMLScenes/AddContractDialog.fxml");
+            dialogPaths.put("AddCustomer", "/FXMLScenes/AddCustomerDialog.fxml");
+            dialogPaths.put("AddTelecast", "/FXMLScenes/AddTelecastDialog.fxml");
+            dialogPaths.put("AddPromo", "/FXMLScenes/AddPromoDialog.fxml");
     }
 
     public static void showScene(String sceneName, Stage stage, Object controller) {
@@ -71,23 +81,38 @@ public class SceneManager {
         AppCache.setLastScene(scene);
     }
 
-    public static void showDialog(String dialogName, Stage ownerStage,Object controller)  {
-        Stage stage = new Stage();
+    public static void showDialog(String dialogName, Stage ownerStage, Object controller)  {
         Parent root = null;
-
         try {
-            FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(SceneManager.class.getResource(dialogName)));
+            FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(SceneManager.class.getResource(dialogPaths.get(dialogName))));
             loader.setController(controller);
             root = loader.load();
         } catch (IOException e) {
             System.err.println("Error loading Dialog" + e.getMessage());
         }
+        Stage dialogStage = new  Stage();
+        dialogStage.initStyle(StageStyle.UNDECORATED);
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(ownerStage);
 
         Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setResizable(false);
-        stage.initOwner(ownerStage);
-        stage.showAndWait();
+        dialogStage.setScene(scene);
+        dialogStage.setResizable(false);
+
+        dialogStage.setOnShown(event -> {
+            double centerX = ownerStage.getX() + ownerStage.getWidth() / 2 - dialogStage.getWidth() / 2;
+            double centerY = ownerStage.getY() + ownerStage.getHeight() / 2 - dialogStage.getHeight() / 2;
+            dialogStage.setX(centerX);
+            dialogStage.setY(centerY);
+        });
+
+        dialogStage.setOnCloseRequest(e -> ownerStage.getScene().getRoot().setStyle(""));
+        dialogStage.showAndWait();
+    }
+
+    public static void closeDialog(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
     }
 }
