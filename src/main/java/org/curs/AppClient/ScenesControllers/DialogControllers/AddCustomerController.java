53ba52d6051a88a9bef397ffd1139810eb755de7
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-15 12:32:36 +0300

    Added starting initialization

---
+++ src/main/java/org/curs/AppClient/ScenesControllers/DialogControllers/AddCustomerController.java
@@ -0,0 +1,15 @@
+package org.curs.AppClient.ScenesControllers.DialogControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import org.curs.AppClient.ScenesControllers.SceneManager;
+
+public class AddCustomerController {
+    @FXML
+    private Button CancelButton ;
+
+    @FXML
+    public void initialize() {
+        CancelButton.setOnAction(SceneManager::closeDialog);
+    }
+}
