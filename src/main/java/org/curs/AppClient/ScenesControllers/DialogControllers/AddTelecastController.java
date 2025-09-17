ddf780d786680eb875b666863ee421489d5ce90b
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-18 01:47:22 +0300

    Added POST

--- src/main/java/org/curs/AppClient/ScenesControllers/DialogControllers/AddTelecastController.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/DialogControllers/AddTelecastController.java
@@ -1,12 +1,17 @@
 package org.curs.AppClient.ScenesControllers.DialogControllers;
 
+import com.google.gson.JsonObject;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputControl;
+import org.curs.AppClient.Enums.ApiPaths;
+import org.curs.AppClient.Enums.ApiRequests;
 import org.curs.AppClient.ScenesControllers.AbstractControllers.AbstractDialogController;
 import org.curs.AppClient.ScenesControllers.SceneManager;
+import org.curs.AppClient.Utils.ApiUtil;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,7 +35,21 @@ public class AddTelecastController extends AbstractDialogController {
 
         AddDialogButton.setOnAction(event -> {
             validate(textFieldRegexMap);
-            if(isValid(textFieldRegexMap)) SceneManager.closeDialog(event);
+            if(isValid(textFieldRegexMap)){
+
+                try {
+                    JsonObject telecastData = new JsonObject();
+                    telecastData.addProperty("telecastName", TelecastNameField.getText());
+                    telecastData.addProperty("rating", TelecastRatingField.getText());
+                    telecastData.addProperty("minuteCost", TelecastMinutePrice.getText());
+
+                    ApiUtil.bodyRequest(ApiPaths.POST_TELECAST, ApiRequests.POST,telecastData );
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                SceneManager.closeDialog(event);
+            }
         });
 
         TelecastNameField.setOnMouseClicked(event -> resetFromError(TelecastNameField));
