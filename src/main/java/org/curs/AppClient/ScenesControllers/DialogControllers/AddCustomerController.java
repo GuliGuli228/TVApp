efdd32d5215ac1d988b07725cb3e7118a66cae5f
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-18 01:40:40 +0300

    Added resetOnError to onMouseClick

--- src/main/java/org/curs/AppClient/ScenesControllers/DialogControllers/AddCustomerController.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/DialogControllers/AddCustomerController.java
@@ -19,17 +19,21 @@ public class AddCustomerController extends AbstractDialogController {
     private TextArea CustomerIbanField;
     @FXML
     private TextField CustomerContactPersonField;
+    @FXML
+    private TextField CustomerName;
 
     @FXML
     public void initialize() {
         CancelDialogButton.setOnAction(SceneManager::closeDialog);
 
         Map<TextInputControl, String> textFieldRegexMap = new HashMap<>();
+        //FIXME почему то валидацию проходит только 8 916 241 30 81
         /* +712345678 or +7-123-456-78-90 or +7 123 456 78 90 or 8912345678 or 8-123-456-78-90 or 8 123 456 78 90 */
         textFieldRegexMap.put(CustomerPhoneNumber,"(\\+7|8)(\\d{8}|(-\\d{3}){2}(-\\d{2}){2}|(\\s\\d{3}){2}(\\s\\d{2}){2})" );
         textFieldRegexMap.put(CustomerIbanField, "\\d{15,34}");
         /*One, Two or Tree words(Latin and Cyrillic)*/
         textFieldRegexMap.put(CustomerContactPersonField, "[\\p{L}]+(\\s[\\p{L}]+){0,2}");
+        textFieldRegexMap.put(CustomerName, "\\w+");
 
         AddDialogButton.setOnAction(event -> {
             validate(textFieldRegexMap);
@@ -50,9 +54,9 @@ public class AddCustomerController extends AbstractDialogController {
                 SceneManager.closeDialog(event);
             }
         });
-        resetFromError(CustomerPhoneNumber);
-        resetFromError(CustomerIbanField);
-        resetFromError(CustomerContactPersonField);
-
+        CustomerPhoneNumber.setOnMouseClicked(event -> {resetFromError(CustomerPhoneNumber);});
+        CustomerContactPersonField.setOnMouseClicked(event -> {resetFromError(CustomerContactPersonField);});
+        CustomerIbanField.setOnMouseClicked(event -> {resetFromError(CustomerIbanField);});
+        CustomerName.setOnMouseClicked(event -> {resetFromError(CustomerName);});
     }
 }
