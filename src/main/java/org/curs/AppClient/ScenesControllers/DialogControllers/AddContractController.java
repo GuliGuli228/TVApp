d19ae7d476afe0dd9a06b1275ba57035f87f8fb9
GuliGuli228 <128092251+GuliGuli228@users.noreply.github.com>
2025-09-18 00:10:18 +0300

    deleted unnecessary imports

--- src/main/java/org/curs/AppClient/ScenesControllers/DialogControllers/AddContractController.java
+++ src/main/java/org/curs/AppClient/ScenesControllers/DialogControllers/AddContractController.java
@@ -2,13 +2,11 @@ package org.curs.AppClient.ScenesControllers.DialogControllers;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.curs.AppClient.AppCache;
 import org.curs.AppClient.Enums.ApiPaths;
